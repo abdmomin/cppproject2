6e6312a6ce8fad5e71932b02d0d32addeb74d310
Abdullah Al Momin <abd.almomin@gmail.com>
2019-07-31 15:01:11 +0800

    Initial Commit

---
+++ employee_payroll/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  employee_payroll
+//
+//  Created by Abdullah Al Momin on 31/07/2019.
+//  Copyright © 2019 Abdullah Al Momin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
